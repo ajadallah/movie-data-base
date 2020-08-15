f25bc2b3cac3878fa97f6b3069adfe1be1aa84cf
Aref <arefajadallah@gmail.com>
2020-08-15 16:14:35 -0400

    Add PropTypes and validation to Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Movie extends Component {
+    static propTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+        }),
+        description: PropTypes.string
+    }
+
+    static defaultProps = {
+        description: 'Description is not available'
+    }
+
     render() {
         return (
             <div>
-                {this.props.movie.title}
+                <h3>{this.props.movie.title}</h3>
+                <p>{this.props.description}</p>
             </div>
         );
     }
