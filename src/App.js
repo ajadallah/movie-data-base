8862e1a427a5369115e3d29e80df3ed63fd7e614
Aref <arefajadallah@gmail.com>
2020-08-15 15:40:47 -0400

    Create Movie component and replace logo picture

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: "The Fellowship of the Ring"
+  },
+  {
+    id: 2,
+    title: "The Two Towers"
+  },
+  {
+    id: 3,
+    title: "The Return of the King"
+  },
+  {
+    id: 4,
+    title: "Iron Man"
+  }
+];
 
 class App extends Component {
-
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value
-    });
-    console.log(this.state.input)
-  }
-
-  submit = () => {
-    console.log(this.text.value);
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Welcome text="Welcome to Using Props u n00b" />
-          </a>
-          <span>
-            <input type='text' onChange={this.updateInput} value={this.state.input} />
-            <input type='text' ref={(input) => this.text = input} />
-            <button onClick={this.submit}>Show Value</button>
-          </span>
         </header>
+        {movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
       </div>
-    );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <h1 className='App-title'>{text}</h1>
     );
   }
 }
