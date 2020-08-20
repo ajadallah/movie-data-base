f7fe02a891ac814f5b58c0ea12c9520df3f94ca5
Aref <arefajadallah@gmail.com>
2020-08-20 19:59:23 -0400

    Refactor project and add linting

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import logo from './logo.png';
 import './App.css';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link
+  Link,
 } from 'react-router-dom';
+import logo from './logo.svg';
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Link to='/'>
-            <img src={logo} className="App-logo" alt="logo" />
-          </Link>
-        </header>
-        <Switch>
-          <Route exact path='/' component={MoviesList} />
-          <Route path='/:id' component={MovieDetail} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,32 +1,40 @@
+/* eslint-disable linebreak-style */
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Movie from './Movie';
 
 class MoviesList extends Component {
-
     state = {
-        movies: []
+      movies: [],
     }
 
     async componentDidMount() {
-        try {
-            const result = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=cbc6a4fcf31d82d2599fd3177bad7130&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-            const movies = await result.json();
-            this.setState({
-                movies: movies.results
-            })
-        } catch (e) {
-            console.log(e);
-        }
+      try {
+        const result = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=cbc6a4fcf31d82d2599fd3177bad7130&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+        const movies = await result.json();
+        this.setState({
+          movies: movies.results,
+        });
+      } catch (e) {
+        console.log(e);
+      }
     }
 
     render() {
-        return (
-            <div>
-                {this.state.movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
-            </div>
-        );
+      return (
+        <MovieGrid>
+          {this.state.movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
+        </MovieGrid>
+      );
     }
 }
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+    display: grid;
+    padding: 1rem;
+    grid-template-columns: repeat(6, 1fr);
+    grid-row-gap: 1rem;
+    `;
