a9574f9102ec081dea97bb0a7e819d4b1e7cf55e
Aref <arefajadallah@gmail.com>
2020-09-01 20:47:58 -0400

    Update Toggle, Update reducer, Add Actions

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Toggle = ({ messageVisibility }) => (
+import { toggleMessage } from './actions';
+
+const Toggle = ({ messageVisibility, dispatch }) => (
   <div>
     {messageVisibility
       && <p>Redux action is toggled</p>}
-    <button>Toggle Me</button>
+    <button
+      type="button"
+      onClick={() => dispatch(toggleMessage())}
+    >
+      Toggle Me
+
+    </button>
   </div>
 );
 
@@ -13,3 +22,8 @@ const mapStateToProps = (state) => ({
   messageVisibility: state.message.messageVisibility,
 });
 export default connect(mapStateToProps)(Toggle);
+
+Toggle.propTypes = {
+  messageVisibility: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
